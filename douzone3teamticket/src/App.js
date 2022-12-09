5eae634695a9bdcb698a1eeee38fa606a09366a9
minnux2 <alstjr1156@naver.com>
2022-12-09 11:15:11 +0900

    뮤지컬 카테고리 메인페이지 css 수정

--- douzone3teamticket/src/App.js
+++ douzone3teamticket/src/App.js
@@ -8,17 +8,19 @@ import Concert from "./Category/Concert";
 import Theater from "./Category/Theater";
 
 import './App.css';
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Musical from "./Category/Musical";
 
 function App() {
     return (
         <div className="App">
             <MainNav></MainNav>
             <Routes>
-                <Route path='/' element={<MainBody></MainBody>}/>
+                <Route path='/' element={<MainBody></MainBody>} />
                 {/* <Route path='/concert' element={<CategoryMain/>}/> */}
-                <Route path='/concert' element={<Concert/>}/>
-                <Route path='/theater' element={<Theater/>}/>
+                <Route path='/concert' element={<Concert />} />
+                <Route path='/musical' element={<Musical />} />
+                <Route path='/theater' element={<Theater />} />
             </Routes>
             <Footer></Footer>
         </div>
