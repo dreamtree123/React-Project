67dc03d35d0ebf3af97d8c815f8e3503922adc8e
HongGunHo05 <booklike112@gmail.com>
2022-12-12 16:41:28 +0900

    서버에 리엑트 파일 추가

---
+++ 3teamserver/douzone3teamticket/src/DetailComponents/DetailTest.js
@@ -0,0 +1,77 @@
+import { useDispatch, useSelector } from "react-redux";
+import concert from "../store";
+import {useEffect} from "react";
+import '../css/Detailheader.css';
+
+function DetailTest() {
+
+    let state = useSelector((state) => state)
+
+    let find = state.classic[state.categoryId]
+
+    return (
+
+        <div>
+            
+                        <div className="renew-wrap">
+                            <div className="renew-content">
+
+                                <div className="rn-02">
+                                    <p className="rn-big-title">{find.title}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="movie-pop-wrap">
+                        </div>
+                        <div className="renew-wrap rw2">
+                            <div className="renew-content">
+                                <div className="rn-03">
+                                    <div className="rn-03-left">
+                                        <div className="rn-product-imgbox">
+                                            <img src={find.imageadr} />
+                                        </div>
+                                    </div>
+                                    <div className="rn-03-right">
+                                        <div className="rn-product-area1">
+                                            <dl>
+                                                <dt>등급</dt>
+                                                <dd>&nbsp;{find.rate}</dd>
+                                                <dt>관람시간</dt>
+                                                <dd>&nbsp;{find.showtime}</dd>
+                                                <dt>출연</dt>
+                                                <dd>&nbsp;<a className='rn-product-peole'>{find.cast}</a></dd>
+                                                <dt id="dtPrice">가격</dt>
+                                                <dd id="divPrice" className="rn-product-price">
+                                                    <ul className="rn-product-price1">
+                                                        <li>VIP석 <span className='rn-red'>{find.priceVIP}</span>원</li>
+                                                        <li>SR석 <span className='rn-red'>{find.priceSR}</span>원</li>
+                                                        <li>R석 <span className='rn-red'>{find.priceR}</span>원</li>
+                                                        <li>S석 <span className='rn-red'>{find.priceS}</span>원</li>
+                                                    </ul>
+                                                </dd>
+                                                <dd className="rn-product-dc"></dd>
+                                            </dl>
+                                        </div>
+
+                                        <div className="rn-product-area3">
+                                            <dl>
+                                                <dt>공연시간 안내</dt>
+                                                <dd>{find.showyear}년 {find.showmonth}월 2{find.showday}일 오후 {find.showtime}시</dd>
+                                                <dt id="deliveryTitle">배송정보</dt>
+                                                <dd>{find.delivery}</dd>
+                                            </dl>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div className="rn-05">
+                                    <a href='src/DetailComponents/Concertdetailheader' onClick='' className='rn-bb03'>예매하기</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                 )
+}
+
+export default DetailTest;
---
+++ 3teamserver/douzone3teamticket/src/UserComponents/Mypage.js
@@ -0,0 +1,104 @@
+import {Route, Routes, useNavigate} from "react-router-dom";
+import '../css/etcCss.css'
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+function Mypage(){
+
+    let navigate = useNavigate();
+
+    let state = useSelector((state) => {
+        return state;
+    });
+
+    let [check, setCheck] = useState(0);
+
+    return(
+        <div className={"mypagebody"}>
+
+            <div className="card">
+                <div className="card-header">
+                    Menu
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item" onClick={ ()=>{setCheck(0)} }>계정관리</li>
+                    <li className="list-group-item" onClick={ ()=>{setCheck(1)} }>예매정보관리</li>
+                    <li className="list-group-item" onClick={ ()=>{setCheck(2)} }>문의하기</li>
+                </ul>
+                <button type="button" className="btn btn-dark logout" onClick={ ()=>{
+                    state.check = ["false", ""]
+                    navigate('/');
+                }}>LogOut</button>
+                <button type="button" className="btn btn-dark" onClick={ ()=>{ navigate('/'); }}>장바구니</button>
+            </div>
+
+
+
+            <div className={"tabct"}>
+                <TabContext check={check}/>
+            </div>
+        </div>
+
+    );
+}
+
+function TabContext({check}) {
+    return (
+        <div className={"tabct"}>
+            {[<Profile/>,<Management/>, <Inquiry/>][check]}
+        </div>
+
+    );
+}
+
+function Profile(){
+
+    let state = useSelector((state) => {
+        return state;
+    });
+
+    function whoislogin(){
+
+        let n = state.userInfo
+
+        // console.log(state.userInfo[0].userid)
+
+        for(let i; i = n.length; i++){
+            // console.log(n[i].userid);
+            console.log(n.length)
+        }
+    }
+
+    return(
+        <div>
+            <h1>Profile</h1>
+            <hr/>
+            <div>
+                이메일
+                {/*{whoislogin()}*/}
+            </div>
+
+        </div>
+    );
+}
+
+function Management(){
+    return(
+        <div>
+            <h1>Management</h1>
+            <hr/>
+        </div>
+    );
+}
+
+function Inquiry(){
+    return(
+        <div>
+            <h1>Inquiry</h1>
+            <hr/>
+        </div>
+    );
+}
+
+export default Mypage;
+
